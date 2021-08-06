262fa80430021478d4c5a3ba233425f66b45c1ee
noowah <henrynoowah@gmail.com>
2021-08-07 02:09:32 +0900

    'bj_1250_python_SOLVED'

--- baekjoon/python/bj_1260.py
+++ baekjoon/python/bj_1260.py
@@ -1,12 +1,15 @@
-arr = [int(v) for v in list(input().split())]
+# arr = [int(v) for v in list(input().split())]
 
-N = arr[0] # 4
-M = arr[1] # 5
-V = arr[2] # 1
+# N = arr[0] # 4
+# M = arr[1] # 5
+# V = arr[2] # 1
+from collections import deque
 
-graph = [[0] * (M) for _ in range(M)]
+N, M, V = map(int, input().split())
+
+graph = [[0] * (N + 1) for _ in range(N + 1)]
 # [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-visited = [False for _ in range(M)]
+visited = [0] * (N + 1)
 # [False, False, False, False, False]
 
 # print(graph)
@@ -16,46 +19,43 @@ visited = [False for _ in range(M)]
 for i in range(M):
 
     x, y = map(int, input().split())
-    graph[x][y] = 1
-    graph[y][x] = 1
+    graph[x][y] = graph[y][x] = 1
 
     # [[0, 0, 0, 0, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 1], [0, 1, 0, 0, 1], [0, 1, 1, 1, 0]]
 
-    # print(graph)
-
 def dfs(V):
-    visited[V] = True
+    visited[V] = 1
     print(V, end = " ")
     # 방문한 값은 True로 변환 후 출력
-    for i in range(1, M):
-        if not visited[i] and graph[V][i] == 1:
+    for i in range(1, (N + 1)):
+        if visited[i] == 0 and graph[V][i] == 1:
             dfs(i)
     # 1 부터 N 까지의 길이의 반복문
+    # True가 된 이후부터 메소드를 다시 시작하는 시작점인 i 를 재귀함수를 통해 파라미터 반환
 
 def bfs(V):
-    visited[V] = False
-    # dfs에서 True가 된 값을 False로 재정의
-    queue = [V]
-    # fase 재정의 후 큐에 반환
-    while queue:
-        V = queue.pop(0)
-        print(V, end = " ")
-        for i in range(1, M):
-            if visited[i] and graph[V][i] == 1:
-                queue.append(i)
-                visited[i] = False
 
-    for i in range(1, M):
-        if graph[V][i] == 1:
-            visited[i] = True
+    queue = deque()
+    queue.append(V)
+    visited[V] = 1
+
+    while queue:
+        V = queue.popleft()
+        print(V, end = " ")
+        # 시작점 V 출력
+
+        for i in range(1, (N + 1)):
+            # 1 ~ 4 까지
+            if visited[i] == 0 and graph[V][i] == 1:
+                # [[0, 0, 0, 0, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 1], [0, 1, 0, 0, 1], [0, 1, 1, 1, 0]]
+                queue.append(i)
+                visited[i] = 1
 
 
 # dfs(V)
 dfs(V)
+visited = [0] * (N + 1)
 print()
 bfs(V)
 
-#   [[1, 2], [1, 3], [1, 4]]
-#   [[2, 4], [3, 4]]
-
-# dequeue rotate 사용??
+# https://kils-log-of-develop.tistory.com/169
