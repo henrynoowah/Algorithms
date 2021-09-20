e00a670ae0498af14df934cd0300bf98fe3f0dd1
noowah <henrynoowah@gmail.com>
2021-09-21 01:42:21 +0900

    [SOLVED]Programmers_구명보트_PYTHON

---
+++ programmers/탐욕법/python/구명보트.py
@@ -0,0 +1,16 @@
+def solution(people, limit):
+    answer = 0
+    people.sort(reverse=True)
+    
+    i = 0
+    j = len(people)-1
+    
+    while i <= j:
+        if people[i] + people[j] > limit:
+            answer += 1
+        else:
+            answer += 1
+            j -= 1
+        i += 1
+
+    return answer
